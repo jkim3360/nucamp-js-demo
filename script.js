abc1e5d7fdfb9c2054526ddfdaca22f88e535b57
James Kim <jamesdaehokim@gmail.com>
2022-06-03 23:13:41 -0400

    init commmit

---
+++ script.js
@@ -0,0 +1,67 @@
+// RENDER ON DOM
+
+// let number = 12
+// function multiply(x, y) {
+//   console.log(x * y * 12)
+// }
+
+// function render(result) {
+//   const container = document.querySelector('.container-2')
+//   container.textContent = result
+//   // console.log(result)
+// }
+
+// render(multiply(4, 8))
+
+// BUTTON ONCLICK
+// function click() {
+//   console.log('clicked')
+//   document.getElementById('container-1').innerHTML = 'clicked'
+//   document.getElementById('container-1').style.backgroundColor = 'red'
+// }
+
+// // WHILE LOOPS
+// let i = 0
+
+// while (i < 5) {
+//   i++
+//   console.log(i)
+// }
+
+// while (i) {
+//   console.log('Got in the lopp')
+// }
+
+// do {
+//   console.log('Got in the loop')
+// } while (i)
+
+// ARRAYS
+
+// let arr = [
+//   'blue',
+//   'cyan',
+//   'gold',
+//   'gray',
+//   'green',
+//   'magenta',
+//   'orange',
+//   'red',
+//   'white',
+//   'yellow'
+// ]
+
+// console.log(arr)
+
+// arr.push('apples')
+// arr.pop()
+// arr.unshift('watermelons')
+// arr.shift()
+// console.log(arr.join(' '))
+// console.log(arr.join(''))
+
+// console.log('includes() example: ', arr.includes('berries'))
+// console.log('indexOf() example: ', arr.indexOf('berries'))
+
+// STRING INDEX
+// console.log('indexOf() on a string: ', 'hello world'.indexOf('l'))
